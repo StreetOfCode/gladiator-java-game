d7bf79acd09c37e3ee2aa1016a9e2725114105a0
xjahic <jahic.jakub@gmail.com>
2023-10-19 11:33:27 +0200

    Setup game cycle

--- src/main/java/sk/streetofcode/constant/Constants.java
+++ src/main/java/sk/streetofcode/constant/Constants.java
@@ -3,4 +3,6 @@ package sk.streetofcode.constant;
 public class Constants {
     public static final int INITIAL_ABILITY_POINTS = 7;
     public static final int HEALTH_OF_ONE_POINT = 5;
+
+    public static final int INITIAL_LEVEL = 1;
 }
--- src/main/java/sk/streetofcode/service/GameManager.java
+++ src/main/java/sk/streetofcode/service/GameManager.java
@@ -1,6 +1,7 @@
 package sk.streetofcode.service;
 
 import sk.streetofcode.ability.HeroAbilityManager;
+import sk.streetofcode.constant.Constants;
 import sk.streetofcode.domain.Hero;
 import sk.streetofcode.utility.InputUtils;
 import sk.streetofcode.utility.PrintUtils;
@@ -8,17 +9,59 @@ import sk.streetofcode.utility.PrintUtils;
 public class GameManager {
     private final HeroAbilityManager heroAbilityManager;
     private final Hero hero;
+    private int currentLevel;
 
     public GameManager() {
         this.hero = new Hero("");
         this.heroAbilityManager = new HeroAbilityManager(this.hero);
+        this.currentLevel = Constants.INITIAL_LEVEL;
     }
 
-    public void startGame()  {
+    public void startGame() {
+        this.initGame();
+
+        while (this.currentLevel <= 5) {
+            System.out.println("0. Fight " + "Level " + this.currentLevel);
+            System.out.println("1. Upgrade abilities (" + this.hero.getHeroAvailablePoints() + " points left)");
+            System.out.println("2. Save game");
+            System.out.println("3. Exit game");
+
+            final int choice = InputUtils.readInt();
+            switch (choice) {
+                case 0 -> {
+                    // TODO fight
+                    this.currentLevel++;
+                }
+                case 1 -> {
+                    // TODO upgrade abilities
+                }
+                case 2 -> {
+                    // TODO save game
+                }
+                case 3 -> {
+                    System.out.println("Are you sure?");
+                    System.out.println("0. No");
+                    System.out.println("1. Yes");
+                    final int exitChoice = InputUtils.readInt();
+                    if (exitChoice == 1) {
+                        System.out.println("Bye!");
+                        return;
+                    }
+                    System.out.println("Continuing game...");
+                    PrintUtils.printDivider();
+                }
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+
+        System.out.println("You have won the game! Congratulations!");
+    }
+
+    private void initGame() {
         System.out.println("Welcome to the Gladiatus game!");
         System.out.println("Enter your name: ");
         final String name = InputUtils.readString();
-        final Hero hero = new Hero(name);
+        this.hero.setName(name);
         System.out.println("Hello " + hero.getName() + "! Let's start the game!");
         PrintUtils.printDivider();
         System.out.println("Your abilities are:");
